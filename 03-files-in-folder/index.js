8b3ed06ff40ace2fd8023bf577b53ac81db91397
sidorenko konstantin <kosiak5587@gmail.com>
2023-05-04 12:14:22 +0600

    feat: add task 3 solution

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+const folderPath = path.join(__dirname, 'secret-folder');
+
+fs.readdir(folderPath, (err, files) => {
+  if (err) throw err;
+  files.forEach((file) => {
+    const filePath = path.join(folderPath, file);
+
+    fs.stat(filePath, (err, stats) => {
+      if (err) throw err;
+
+      if (stats.isFile()) {
+        const name = path.basename(file, path.extname(file));
+        const extname = path.extname(file).slice(1);
+        const size = stats.size / 1024;
+        console.log(`${name}-${extname}-${size.toFixed(2)}кб.`);
+      }
+    });
+  });
+});
